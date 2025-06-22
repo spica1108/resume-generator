0e4df40acd0d54bb3cabbcc570f7d91e6f66542b
spica1108 <815303067@qq.com>
2025-06-22 16:06:48 +0800

    描述本次修改内容

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ResumeForm from './components/ResumeForm'
 import ResumePreview from './components/ResumePreview'
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    education: '',
-    skill: '',
-    campusExperience: '',
-    projects: '',
-    socialPractice: '',
-    internships: '',
-    awards: '',
-    selfIntro: ''
-  })
-
+  // 浏览器的内置存储空间，getitem从存储中取出名为"resumeData"的东西，
+  // const storedData = - 把取出的东西赋值给变量storedData
+  const storedData = localStorage.getItem('resumeData')
+  const [formData, setFormData] = useState(
+    // storeddata如果存在，就把字符串转化为对象，
+    // 把{"name":"小明"}字符串，变成真正的JS对象{name:"小明"}
+    storedData ? JSON.parse(storedData) : {
+      name: '', email: '', education: '', skill: '', campusExperience: '', projects: '', socialPractice: '', internships: '', awards: '', selfIntro: ''
+    })
+  // JSON.stringify把 JS对象或值转换成JSON字符串格式，
+  // localStorage（只能存字符串，不能直接存对象）才能运输和存储
+  useEffect(() => {
+    localStorage.setItem('resumeData', JSON.stringify(formData))
+  }, [formData])
 
   return (
     <div>
--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -23,25 +23,30 @@ export default function ResumePreview({ formData }) {
     <div>
       {/* 如果 formData.email有值，就会显示这个邮箱地址
        null、undefined 或""，就会显示默认值邮箱
-       || 常用于数据未填写时显示默认值 */}
-      <h2>{formData.name || '姓名'}</h2>
-      <p>{formData.email || '邮箱'}</p>
-      <h4>教育经历</h4>
-      <p>{formData.education || '请填写你的教育经历...'}</p>
-      <h4>专业技能</h4>
-      <p>{formData.skill || '请填写你的专业技能...'}</p>
-      <h4>校内经历</h4>
-      <p>{formData.campusExperience || '请填写你的校内经历...'}</p>
-      <h4>项目经历</h4>
-      <p>{formData.projects || '请填写你的项目经历...'}</p>
-      <h4>社会实践</h4>
-      <p>{formData.socialPractice || '请填写你的社会实践...'}</p>
-      <h4>实习经历</h4>
-      <p>{formData.internships || '请填写你的实习经历..'}</p>
-      <h4>奖项荣誉</h4>
-      <p>{formData.awards || '请填写你的奖项荣誉...'}</p>
-      <h4>自我评价</h4>
-      <p>{formData.selfIntro || '请填写你的自我评价...'}</p>
+       || 常用于数据未填写时显示默认值 
+        内边距16px（上下左右），1px灰色边框，圆角4px，轻微阴影，白色背景*/}
+      <div id='resume-preview' className='p-4 border rounded shadow bg-white' style={{ maxWidth: '700px', margin: '0 auto' }}>
+
+        <h2 className='fw-bold border-buttom pd-2'>{formData.name || '姓名'}</h2>
+        <p>{formData.email || '邮箱'}</p>
+        <h4 className='mt-3 fw-bold'>🎓 教育经历</h4>
+        <p>{formData.education || '请填写...'}</p>
+        <h4 className='mt-3 fw-bold'>🛠️ 专业技能</h4>
+        <p>{formData.skill || '请填写...'}</p>
+        <h4 className='mt-3 fw-bold'>🏫 校内经历</h4>
+        <p>{formData.campusExperience || '请填写...'}</p>
+        <h4 className='mt-3 fw-bold'>💻 项目经历</h4>
+        <p>{formData.projects || '请填写...'}</p>
+        <h4 className='mt-3 fw-bold'>🌏 社会实践</h4>
+        <p>{formData.socialPractice || '请填写...'}</p>
+        <h4 className='mt-3 fw-bold'>🏢 实习经历</h4>
+        <p>{formData.internships || '请填写..'}</p>
+        <h4 className='mt-3 fw-bold'>🏅 奖项荣誉</h4>
+        <p>{formData.awards || '请填写...'}</p>
+        <h4 className='mt-3 fw-bold'>📝 自我评价</h4>
+        <p>{formData.selfIntro || '请填写...'}</p>
+      </div>
+
       <button onClick={exportPDF}>
         导出 PDF
       </button>
